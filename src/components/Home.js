cd0f18ae1a24d2c8ce98c600bf67d497165e5581
Poojanavgurukul <pooja17@navgurukul.org>
2020-03-19 15:44:09 +0530

    added the loader and courses

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Nav from './Nav';
+import CoursesList from './CoursesList';
+import './style.css'
+
 function Home() {
   return (
     <div>
      <Nav />
+    <CoursesList />
     </div>
   );
 }
